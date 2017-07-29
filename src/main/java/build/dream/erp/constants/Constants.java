c44d2271aa3f8de990a8dfadd32ee19f51e85173
liuyandong <554030404@qq.com>
2017-07-29 15:33:21 +0800

    加入bootstrap-table！！

---
+++ src/main/java/build/dream/erp/constants/Constants.java
@@ -0,0 +1,11 @@
+package build.dream.erp.constants;
+
+/**
+ * Created by liuyandong on 2017/5/5.
+ */
+public class Constants {
+    public static final String SUCCESS = "SUCCESS";
+    public static final String FAILURE = "FAILURE";
+
+    public static final String DEFAULT_DATE_PATTERN = "yyyy-MM-dd HH:mm:ss";
+}
