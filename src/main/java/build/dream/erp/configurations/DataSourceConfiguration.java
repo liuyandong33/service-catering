864ddbb570cf35bac73ddbedf07b81810a16e35f
liuyandong <554030404@qq.com>
2017-09-14 11:46:42 +0800

    动态数据源！！！

--- src/main/java/build/dream/erp/configurations/DataSourceConfiguration.java
+++ src/main/java/build/dream/erp/configurations/DataSourceConfiguration.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
 import javax.sql.DataSource;
 
