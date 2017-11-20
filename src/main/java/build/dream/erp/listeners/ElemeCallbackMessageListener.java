11f170d18a8a2232d05fb99ebcf664ccff4014d2
liuyandong <554030404@qq.com>
2017-11-20 13:48:10 +0800

    错误修改！！！

--- src/main/java/build/dream/erp/listeners/ElemeCallbackMessageListener.java
+++ src/main/java/build/dream/erp/listeners/ElemeCallbackMessageListener.java
@@ -1,13 +1,17 @@
 package build.dream.erp.listeners;
 
+import build.dream.common.utils.CacheUtils;
 import build.dream.common.utils.LogUtils;
 import build.dream.erp.utils.ElemeUtils;
+import net.sf.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.MessageListener;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
 
 @Component
 public class ElemeCallbackMessageListener implements MessageListener {
@@ -19,10 +23,16 @@ public class ElemeCallbackMessageListener implements MessageListener {
     public void onMessage(Message message, byte[] pattern) {
         RedisSerializer<String> stringRedisSerializer = stringRedisTemplate.getStringSerializer();
         String body = stringRedisSerializer.deserialize(message.getBody());
-        try {
-            ElemeUtils.addElemeMessageBlockingQueue(body, 10);
-        } catch (InterruptedException e) {
-            LogUtils.error("处理饿了么回调信息失败", ELEME_CALLBACK_MESSAGE_LISTENER_SIMPLE_NAME, "onMessage", e);
+        JSONObject messageJsonObject = JSONObject.fromObject(body);
+        String uuid = messageJsonObject.getString("uuid");
+        boolean setnxSuccessful = CacheUtils.setnx(uuid, uuid);
+        if (setnxSuccessful) {
+            CacheUtils.expire(uuid, 1800, TimeUnit.SECONDS);
+            try {
+                ElemeUtils.addElemeMessageBlockingQueue(messageJsonObject.getString("callbackRequestBody"), 10);
+            } catch (InterruptedException e) {
+                LogUtils.error("处理饿了么回调信息失败", ELEME_CALLBACK_MESSAGE_LISTENER_SIMPLE_NAME, "onMessage", e);
+            }
         }
     }
 }
