84a56a59f659006a9dd05b2c1988e7d203a30d3f
liuyandong <554030404@qq.com>
2017-11-20 13:53:29 +0800

    动态数据源！！！

--- src/main/java/build/dream/erp/listeners/MeiTuanCallbackMessageListener.java
+++ src/main/java/build/dream/erp/listeners/MeiTuanCallbackMessageListener.java
@@ -1,8 +1,9 @@
 package build.dream.erp.listeners;
 
+import build.dream.common.utils.CacheUtils;
 import build.dream.common.utils.LogUtils;
-import build.dream.erp.utils.ElemeUtils;
 import build.dream.erp.utils.MeiTuanUtils;
+import net.sf.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.MessageListener;
@@ -20,10 +21,15 @@ public class MeiTuanCallbackMessageListener implements MessageListener {
     public void onMessage(Message message, byte[] pattern) {
         RedisSerializer<String> stringRedisSerializer = stringRedisTemplate.getStringSerializer();
         String body = stringRedisSerializer.deserialize(message.getBody());
-        try {
-            MeiTuanUtils.addMeiTuanMessageBlockingQueue(body, 10);
-        } catch (InterruptedException e) {
-            LogUtils.error("处理美团回调信息失败", MEI_TUAN_CALLBACK_MESSAGE_LISTENER_SIMPLE_NAME, "onMessage", e);
+        JSONObject messageJsonObject = JSONObject.fromObject(body);
+        String uuid = messageJsonObject.getString("uuid");
+        boolean setnxSuccessful = CacheUtils.setnx(uuid, uuid);
+        if (setnxSuccessful) {
+            try {
+                MeiTuanUtils.addMeiTuanMessageBlockingQueue(messageJsonObject.getString("callbackRequestBody"), 10);
+            } catch (InterruptedException e) {
+                LogUtils.error("处理美团回调信息失败", MEI_TUAN_CALLBACK_MESSAGE_LISTENER_SIMPLE_NAME, "onMessage", e);
+            }
         }
     }
 }
