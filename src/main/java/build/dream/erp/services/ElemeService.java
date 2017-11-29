ed0701ce6392800737fa20e6348a734b1c75aa48
liuyandong <554030404@qq.com>
2017-11-29 22:39:20 +0800

    饿了么对接！！！

--- src/main/java/build/dream/erp/services/ElemeService.java
+++ src/main/java/build/dream/erp/services/ElemeService.java
@@ -21,7 +21,6 @@ import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.net.URLEncoder;
 import java.util.*;
-import java.util.concurrent.TimeUnit;
 
 @Service
 public class ElemeService {
