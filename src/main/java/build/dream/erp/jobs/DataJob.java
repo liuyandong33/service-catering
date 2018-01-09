48b7bd8cf6006d38ff34ce3927b32ed7d19900d7
liuyandong <554030404@qq.com>
2018-01-09 20:00:00 +0800

    aaa

--- src/main/java/build/dream/erp/jobs/DataJob.java
+++ src/main/java/build/dream/erp/jobs/DataJob.java
@@ -1,17 +1,22 @@
 package build.dream.erp.jobs;
 
+import build.dream.common.utils.CacheUtils;
 import build.dream.common.utils.ConfigurationUtils;
 import build.dream.common.utils.LogUtils;
 import build.dream.common.utils.QueueUtils;
 import build.dream.erp.constants.Constants;
 import build.dream.erp.services.DataService;
+import org.apache.commons.codec.digest.DigestUtils;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.concurrent.TimeUnit;
+
 public class DataJob implements Job {
     private static final String DATA_JOB_SIMPLE_NAME = "DataJob";
     private static String KEY_DIET_ORDER_DATA;
+    private static final String FLAG = DigestUtils.md5Hex(DATA_JOB_SIMPLE_NAME);
     @Autowired
     private DataService dataService;
     static {
@@ -25,11 +30,15 @@ public class DataJob implements Job {
     }
     @Override
     public void execute(JobExecutionContext context) {
-        long length = QueueUtils.llen(KEY_DIET_ORDER_DATA);
-        for (long index = 0; index < length; index++) {
-            String dietOrderData = QueueUtils.lpop(KEY_DIET_ORDER_DATA);
-            System.out.println(dietOrderData);
-            QueueUtils.rpush(KEY_DIET_ORDER_DATA, dietOrderData);
+        boolean setnxSuccessful = CacheUtils.setnx(FLAG, FLAG);
+        if (setnxSuccessful) {
+            CacheUtils.expire(FLAG, 120, TimeUnit.SECONDS);
+            long length = QueueUtils.llen(KEY_DIET_ORDER_DATA);
+            for (long index = 0; index < length; index++) {
+                String dietOrderData = QueueUtils.lpop(KEY_DIET_ORDER_DATA);
+                System.out.println(dietOrderData);
+                QueueUtils.rpush(KEY_DIET_ORDER_DATA, dietOrderData);
+            }
         }
     }
 }
