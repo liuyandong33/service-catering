07c8539ab0afd2aadd4ac84996ef14a3c62b87a5
liuyandong <554030404@qq.com>
2018-01-21 00:56:03 +0800

    修改包名！！

--- src/main/java/build/dream/catering/controllers/DietOrderController.java
+++ src/main/java/build/dream/catering/controllers/DietOrderController.java
@@ -47,8 +47,14 @@ public class DietOrderController extends BasicController {
         ApiRest apiRest = null;
         Map<String, String> requestParameters = ApplicationHandler.getRequestParameters();
         try {
+            String groups = requestParameters.get("groups");
+            ApplicationHandler.notEmpty(groups, "groups");
+
             SaveDietOrderModel saveDietOrderModel = ApplicationHandler.instantiateObject(SaveDietOrderModel.class, requestParameters);
             saveDietOrderModel.validateAndThrow();
+
+            List<SaveDietOrderModel.GroupModel> groupModels = GsonUtils.jsonToList(groups, SaveDietOrderModel.GroupModel.class);
+            saveDietOrderModel.setGroupModels(groupModels);
 
             apiRest = dietOrderService.saveDietOrder(saveDietOrderModel);
         } catch (Exception e) {
--- src/main/java/build/dream/catering/models/dietorder/SaveDietOrderModel.java
+++ src/main/java/build/dream/catering/models/dietorder/SaveDietOrderModel.java
@@ -1,6 +1,152 @@
 package build.dream.catering.models.dietorder;
 
 import build.dream.common.models.BasicModel;
+import build.dream.common.utils.ApplicationHandler;
+import org.apache.commons.collections.CollectionUtils;
+
+import javax.validation.constraints.NotNull;
+import java.math.BigInteger;
+import java.util.List;
 
 public class SaveDietOrderModel extends BasicModel {
+    @NotNull
+    private BigInteger tenantId;
+
+    @NotNull
+    private String tenantCode;
+
+    @NotNull
+    private BigInteger branchId;
+
+    @NotNull
+    private List<GroupModel> groupModels;
+
+    public BigInteger getTenantId() {
+        return tenantId;
+    }
+
+    public void setTenantId(BigInteger tenantId) {
+        this.tenantId = tenantId;
+    }
+
+    public String getTenantCode() {
+        return tenantCode;
+    }
+
+    public void setTenantCode(String tenantCode) {
+        this.tenantCode = tenantCode;
+    }
+
+    public BigInteger getBranchId() {
+        return branchId;
+    }
+
+    public void setBranchId(BigInteger branchId) {
+        this.branchId = branchId;
+    }
+
+    public List<GroupModel> getGroupModels() {
+        return groupModels;
+    }
+
+    public void setGroupModels(List<GroupModel> groupModels) {
+        this.groupModels = groupModels;
+    }
+
+    @Override
+    public boolean validate() {
+        if (!super.validate()) {
+            return false;
+        }
+        if (CollectionUtils.isEmpty(groupModels)) {
+            return false;
+        }
+        for (GroupModel groupModel : groupModels) {
+            if (!groupModel.validate()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public void validateAndThrow() {
+        super.validateAndThrow();
+        ApplicationHandler.notEmpty(groupModels, "groups");
+        for (GroupModel groupModel : groupModels) {
+            ApplicationHandler.isTrue(groupModel.validate(), "groups");
+        }
+    }
+
+    public static class GroupModel extends BasicModel {
+        @NotNull
+        private String name;
+        private List<DetailModel> detailModels;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public List<DetailModel> getDetailModels() {
+            return detailModels;
+        }
+
+        public void setDetailModels(List<DetailModel> detailModels) {
+            this.detailModels = detailModels;
+        }
+
+        @Override
+        public boolean validate() {
+            if (!super.validate()) {
+                return false;
+            }
+            if (CollectionUtils.isEmpty(detailModels)) {
+                return false;
+            }
+            for (DetailModel detailModel : detailModels) {
+                if (!detailModel.validate()) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+    private static class DetailModel extends BasicModel {
+        @NotNull
+        private BigInteger goodsId;
+
+        @NotNull
+        private Integer quantity;
+
+        private List<BigInteger> flavorIds;
+
+        public BigInteger getGoodsId() {
+            return goodsId;
+        }
+
+        public void setGoodsId(BigInteger goodsId) {
+            this.goodsId = goodsId;
+        }
+
+        public Integer getQuantity() {
+            return quantity;
+        }
+
+        public void setQuantity(Integer quantity) {
+            this.quantity = quantity;
+        }
+
+        public List<BigInteger> getFlavorIds() {
+            return flavorIds;
+        }
+
+        public void setFlavorIds(List<BigInteger> flavorIds) {
+            this.flavorIds = flavorIds;
+        }
+    }
 }
