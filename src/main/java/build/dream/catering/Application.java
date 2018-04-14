a72184a8b5b8b25c9dd99aa1fe3fb36aba0979de
liuyandong <554030404@qq.com>
2018-04-14 09:43:10 +0800

    菜品接口！！！

--- src/main/java/build/dream/catering/Application.java
+++ src/main/java/build/dream/catering/Application.java
@@ -6,9 +6,11 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @ServletComponentScan
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableEurekaClient
 public class Application extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
