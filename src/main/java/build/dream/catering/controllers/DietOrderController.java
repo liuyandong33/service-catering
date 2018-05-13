dad1700009c3b0079d8f8dd0855c53e2c7d09ef3
liuyandong <554030404@qq.com>
2018-05-13 14:12:19 +0800

    微信会员卡功能开发！！

--- src/main/java/build/dream/catering/controllers/DietOrderController.java
+++ src/main/java/build/dream/catering/controllers/DietOrderController.java
@@ -3,11 +3,9 @@ package build.dream.catering.controllers;
 import build.dream.catering.models.dietorder.ObtainDietOrderInfoModel;
 import build.dream.catering.models.dietorder.SaveDietOrderModel;
 import build.dream.catering.services.DietOrderService;
-import build.dream.common.api.ApiRest;
 import build.dream.common.controllers.BasicController;
 import build.dream.common.utils.ApplicationHandler;
-import build.dream.common.utils.GsonUtils;
-import build.dream.common.utils.LogUtils;
+import build.dream.common.utils.MethodCaller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,17 +27,13 @@ public class DietOrderController extends BasicController {
     @RequestMapping(value = "/obtainDietOrderInfo")
     @ResponseBody
     public String obtainDietOrderInfo() {
-        ApiRest apiRest = null;
         Map<String, String> requestParameters = ApplicationHandler.getRequestParameters();
-        try {
+        MethodCaller methodCaller = () -> {
             ObtainDietOrderInfoModel obtainDietOrderInfoModel = ApplicationHandler.instantiateObject(ObtainDietOrderInfoModel.class, requestParameters);
             obtainDietOrderInfoModel.validateAndThrow();
-            apiRest = dietOrderService.obtainDietOrderInfo(obtainDietOrderInfoModel);
-        } catch (Exception e) {
-            LogUtils.error("获取订单信息失败", controllerSimpleName, "obtainDietOrderInfo", e, requestParameters);
-            apiRest = new ApiRest(e);
-        }
-        return GsonUtils.toJson(apiRest);
+            return dietOrderService.obtainDietOrderInfo(obtainDietOrderInfoModel);
+        };
+        return ApplicationHandler.callMethod(methodCaller, "获取订单信息失败", requestParameters);
     }
 
     /**
@@ -50,19 +44,15 @@ public class DietOrderController extends BasicController {
     @RequestMapping(value = "/saveDietOrder")
     @ResponseBody
     public String saveDietOrder() {
-        ApiRest apiRest = null;
         Map<String, String> requestParameters = ApplicationHandler.getRequestParameters();
-        try {
+        MethodCaller methodCaller = () -> {
             SaveDietOrderModel saveDietOrderModel = ApplicationHandler.instantiateObject(SaveDietOrderModel.class, requestParameters);
             String groups = requestParameters.get("groups");
             saveDietOrderModel.setGroupInfos(groups);
             saveDietOrderModel.validateAndThrow();
 
-            apiRest = dietOrderService.saveDietOrder(saveDietOrderModel);
-        } catch (Exception e) {
-            LogUtils.error("保存订单失败", controllerSimpleName, "saveDietOrder", e, requestParameters);
-            apiRest = new ApiRest(e);
-        }
-        return GsonUtils.toJson(apiRest);
+            return dietOrderService.saveDietOrder(saveDietOrderModel);
+        };
+        return ApplicationHandler.callMethod(methodCaller, "保存订单失败", requestParameters);
     }
 }
--- src/main/java/build/dream/catering/controllers/MeiTuanController.java
+++ src/main/java/build/dream/catering/controllers/MeiTuanController.java
@@ -4,11 +4,9 @@ import build.dream.catering.models.meituan.CheckIsBindingModel;
 import build.dream.catering.models.meituan.GenerateBindingStoreLinkModel;
 import build.dream.catering.models.meituan.ObtainMeiTuanOrderModel;
 import build.dream.catering.services.MeiTuanService;
-import build.dream.common.api.ApiRest;
 import build.dream.common.controllers.BasicController;
 import build.dream.common.utils.ApplicationHandler;
-import build.dream.common.utils.GsonUtils;
-import build.dream.common.utils.LogUtils;
+import build.dream.common.utils.MethodCaller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,17 +28,13 @@ public class MeiTuanController extends BasicController {
     @RequestMapping(value = "/generateBindingStoreLink")
     @ResponseBody
     public String generateBindingStoreLink() {
-        ApiRest apiRest = null;
         Map<String, String> requestParameters = ApplicationHandler.getRequestParameters();
-        try {
+        MethodCaller methodCaller = () -> {
             GenerateBindingStoreLinkModel generateBindingStoreLinkModel = ApplicationHandler.instantiateObject(GenerateBindingStoreLinkModel.class, requestParameters);
             generateBindingStoreLinkModel.validateAndThrow();
-            apiRest = meiTuanService.generateBindingStoreLink(generateBindingStoreLinkModel);
-        } catch (Exception e) {
-            LogUtils.error("生成门店绑定链接失败", controllerSimpleName, "createCategoryWithChildren", e, requestParameters);
-            apiRest = new ApiRest(e);
-        }
-        return GsonUtils.toJson(apiRest);
+            return meiTuanService.generateBindingStoreLink(generateBindingStoreLinkModel);
+        };
+        return ApplicationHandler.callMethod(methodCaller, "生成门店绑定链接失败", requestParameters);
     }
 
     /**
@@ -51,18 +45,14 @@ public class MeiTuanController extends BasicController {
     @RequestMapping(value = "/obtainMeiTuanOrder")
     @ResponseBody
     public String obtainMeiTuanOrder() {
-        ApiRest apiRest = null;
         Map<String, String> requestParameters = ApplicationHandler.getRequestParameters();
-        try {
+        MethodCaller methodCaller = () -> {
             ObtainMeiTuanOrderModel obtainMeiTuanOrderModel = ApplicationHandler.instantiateObject(ObtainMeiTuanOrderModel.class, requestParameters);
             obtainMeiTuanOrderModel.validateAndThrow();
 
-            apiRest = meiTuanService.obtainMeiTuanOrder(obtainMeiTuanOrderModel);
-        } catch (Exception e) {
-            LogUtils.error("拉取美团订单失败！", controllerSimpleName, "pullMeiTuanOrder", e, requestParameters);
-            apiRest = new ApiRest(e);
-        }
-        return GsonUtils.toJson(apiRest);
+            return meiTuanService.obtainMeiTuanOrder(obtainMeiTuanOrderModel);
+        };
+        return ApplicationHandler.callMethod(methodCaller, "拉取美团订单失败", requestParameters);
     }
 
     /**
@@ -73,17 +63,13 @@ public class MeiTuanController extends BasicController {
     @RequestMapping(value = "/checkIsBinding")
     @ResponseBody
     public String checkIsBinding() {
-        ApiRest apiRest = null;
         Map<String, String> requestParameters = ApplicationHandler.getRequestParameters();
-        try {
+        MethodCaller methodCaller = () -> {
             CheckIsBindingModel checkIsBindingModel = ApplicationHandler.instantiateObject(CheckIsBindingModel.class, requestParameters);
             checkIsBindingModel.validateAndThrow();
 
-            apiRest = meiTuanService.checkIsBinding(checkIsBindingModel);
-        } catch (Exception e) {
-            LogUtils.error("查询门店是否绑定美团失败！", controllerSimpleName, "checkIsBinding", e, requestParameters);
-            apiRest = new ApiRest(e);
-        }
-        return GsonUtils.toJson(apiRest);
+            return meiTuanService.checkIsBinding(checkIsBindingModel);
+        };
+        return ApplicationHandler.callMethod(methodCaller, "查询门店是否绑定美团失败", requestParameters);
     }
 }
