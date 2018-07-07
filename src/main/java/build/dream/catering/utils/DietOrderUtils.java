7859f4c9249d3464b7883114a43ae7b9046e5848
liuyandong <554030404@qq.com>
2018-07-07 23:04:20 +0800

    微信会员卡功能开发！！

--- src/main/java/build/dream/catering/utils/DietOrderUtils.java
+++ src/main/java/build/dream/catering/utils/DietOrderUtils.java
@@ -1,5 +1,28 @@
 package build.dream.catering.utils;
 
-public class DietOrderUtils {
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.ConcurrentHashMap;
 
+public class DietOrderUtils {
+    private static final ConcurrentHashMap<String, Timer> CONCURRENT_HASH_MAP = new ConcurrentHashMap<String, Timer>();
+
+    public static void startTimer(String orderNumber) {
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                CONCURRENT_HASH_MAP.remove(orderNumber);
+                timer.cancel();
+            }
+        }, 0, 1000);
+        CONCURRENT_HASH_MAP.put(orderNumber, timer);
+    }
+
+    public static void stopTimer(String orderNumber) {
+        Timer timer = CONCURRENT_HASH_MAP.remove(orderNumber);
+        if (timer != null) {
+            timer.cancel();
+        }
+    }
 }
