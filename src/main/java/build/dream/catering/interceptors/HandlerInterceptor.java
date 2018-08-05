f67a7930db278bc812a91272db9b399efde36ac1
liuyandong <554030404@qq.com>
2018-08-05 14:05:22 +0800

    微信会员卡功能开发！！

--- src/main/java/build/dream/catering/interceptors/HandlerInterceptor.java
+++ src/main/java/build/dream/catering/interceptors/HandlerInterceptor.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 @Component
 public class HandlerInterceptor implements org.springframework.web.servlet.HandlerInterceptor {
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
         request.setAttribute(Constants.HANDLER_METHOD, handler);
         return true;
     }
