b1e65849c408abf43f5f14329217bf97183d55aa
liuyandong <554030404@qq.com>
2018-10-08 10:37:47 +0800

    菜品接口！！！

--- src/main/java/build/dream/catering/configurations/JobRegistryCenterConfiguration.java
+++ src/main/java/build/dream/catering/configurations/JobRegistryCenterConfiguration.java
@@ -7,12 +7,10 @@ import com.dangdang.ddframe.job.reg.zookeeper.ZookeeperRegistryCenter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.io.IOException;
-
 @Configuration
 public class JobRegistryCenterConfiguration {
     @Bean(initMethod = "init")
-    public ZookeeperRegistryCenter zookeeperRegistryCenter() throws IOException {
+    public ZookeeperRegistryCenter zookeeperRegistryCenter() {
         String serverList = ConfigurationUtils.getConfiguration(Constants.ELASTICJOB_ZOOKEEPER_SERVER_LISTS);
         String namespace = ConfigurationUtils.getConfiguration(Constants.ELASTICJOB_ZOOKEEPER_NAMESPACE);
         return new ZookeeperRegistryCenter(new ZookeeperConfiguration(serverList, namespace));
