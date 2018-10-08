7df6888f8ebec2f4081f9177ac71c99bdb142a50
liuyandong <554030404@qq.com>
2018-10-08 13:41:16 +0800

    菜品接口！！！

--- src/main/java/build/dream/catering/models/user/ObtainUserInfoModel.java
+++ src/main/java/build/dream/catering/models/user/ObtainUserInfoModel.java
@@ -2,7 +2,10 @@ package build.dream.catering.models.user;
 
 import build.dream.common.models.BasicModel;
 
+import javax.validation.constraints.NotNull;
+
 public class ObtainUserInfoModel extends BasicModel {
+    @NotNull
     private String loginName;
 
     public String getLoginName() {
