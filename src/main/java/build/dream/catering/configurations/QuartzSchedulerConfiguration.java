6ad16cbb08b87737ad06f58208e845309df64c3b
liuyandong <554030404@qq.com>
2018-10-14 19:58:04 +0800

    微信会员卡功能开发！！

--- src/main/java/build/dream/catering/configurations/QuartzSchedulerConfiguration.java
+++ src/main/java/build/dream/catering/configurations/QuartzSchedulerConfiguration.java
@@ -5,8 +5,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.scheduling.quartz.AdaptableJobFactory;
 import org.springframework.scheduling.quartz.SchedulerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
 
 /**
  * Created by liuyandong on 2017/09/20.
@@ -14,10 +19,24 @@ import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 
 @Configuration
 public class QuartzSchedulerConfiguration {
+    @Autowired
+    private DataSource dataSource;
+    @Autowired
+    private PlatformTransactionManager platformTransactionManager;
+    public static final String SCHEDULER_NAME = "quartzScheduler";
+    public static final String APPLICATION_CONTEXT = "applicationContext";
+    public static final String QUARTZ_PROPERTIES = "quartz.properties";
+
     @Bean
     public SchedulerFactoryBean schedulerFactoryBean(JobFactory jobFactory) {
         SchedulerFactoryBean schedulerFactoryBean = new SchedulerFactoryBean();
         schedulerFactoryBean.setJobFactory(jobFactory);
+        Resource configLocation = new ClassPathResource(QUARTZ_PROPERTIES);
+        schedulerFactoryBean.setConfigLocation(configLocation);
+        schedulerFactoryBean.setDataSource(dataSource);
+        schedulerFactoryBean.setTransactionManager(platformTransactionManager);
+        schedulerFactoryBean.setSchedulerName(SCHEDULER_NAME);
+        schedulerFactoryBean.setApplicationContextSchedulerContextKey(APPLICATION_CONTEXT);
         return schedulerFactoryBean;
     }
 
