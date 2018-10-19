09ef8aee01645b7d731f29dbc543ffc1f886431f
liuyandong <554030404@qq.com>
2018-10-20 01:44:21 +0800

    错误修改！！

--- src/main/java/build/dream/catering/models/goods/CountModel.java
+++ src/main/java/build/dream/catering/models/goods/CountModel.java
@@ -1,30 +1,7 @@
 package build.dream.catering.models.goods;
 
-import build.dream.common.models.BasicModel;
+import build.dream.common.models.CateringBasicModel;
 
-import javax.validation.constraints.NotNull;
-import java.math.BigInteger;
+public class CountModel extends CateringBasicModel {
 
-public class CountModel extends BasicModel {
-    @NotNull
-    private BigInteger tenantId;
-
-    @NotNull
-    private BigInteger branchId;
-
-    public BigInteger getTenantId() {
-        return tenantId;
-    }
-
-    public void setTenantId(BigInteger tenantId) {
-        this.tenantId = tenantId;
-    }
-
-    public BigInteger getBranchId() {
-        return branchId;
-    }
-
-    public void setBranchId(BigInteger branchId) {
-        this.branchId = branchId;
-    }
 }
