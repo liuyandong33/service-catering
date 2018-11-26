6a2ebb0738ac882cf55c54a1f039320673404aae
liuyandong <554030404@qq.com>
2018-11-26 10:08:33 +0800

    保存进货单接口开发！！

--- src/main/java/build/dream/catering/models/purchase/SavePurchaseOrderModel.java
+++ src/main/java/build/dream/catering/models/purchase/SavePurchaseOrderModel.java
@@ -1,30 +1,114 @@
 package build.dream.catering.models.purchase;
 
 import build.dream.common.models.BasicModel;
+import build.dream.common.models.CateringBasicModel;
+import build.dream.common.utils.ApplicationHandler;
 
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.util.List;
 
-public class SavePurchaseOrderModel extends BasicModel {
-    @NotNull
-    private BigInteger tenantId;
+public class SavePurchaseOrderModel extends CateringBasicModel {
+    private String remark;
 
-    @NotNull
-    private BigInteger branchId;
+    @NotEmpty
+    private List<Detail> details;
 
-    public BigInteger getTenantId() {
-        return tenantId;
+    public String getRemark() {
+        return remark;
     }
 
-    public void setTenantId(BigInteger tenantId) {
-        this.tenantId = tenantId;
+    public void setRemark(String remark) {
+        this.remark = remark;
     }
 
-    public BigInteger getBranchId() {
-        return branchId;
+    public List<Detail> getDetails() {
+        return details;
     }
 
-    public void setBranchId(BigInteger branchId) {
-        this.branchId = branchId;
+    public void setDetails(List<Detail> details) {
+        this.details = details;
+    }
+
+    @Override
+    public boolean validate() {
+        boolean isOk = super.validate();
+        if (!isOk) {
+            return false;
+        }
+        for (Detail detail : details) {
+            isOk = isOk && detail.validate();
+            if (!isOk) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public void validateAndThrow() {
+        super.validateAndThrow();
+        for (Detail detail : details) {
+            ApplicationHandler.isTrue(detail.validate(), "details");
+        }
+    }
+
+    public static class Detail extends BasicModel {
+        @NotNull
+        private BigInteger goodsId;
+
+        @NotNull
+        private BigInteger goodsSpecificationId;
+
+        @NotNull
+        private BigInteger unitId;
+
+        @NotNull
+        private BigDecimal purchasePrice;
+
+        @NotNull
+        private BigDecimal quantity;
+
+        public BigInteger getGoodsId() {
+            return goodsId;
+        }
+
+        public void setGoodsId(BigInteger goodsId) {
+            this.goodsId = goodsId;
+        }
+
+        public BigInteger getGoodsSpecificationId() {
+            return goodsSpecificationId;
+        }
+
+        public void setGoodsSpecificationId(BigInteger goodsSpecificationId) {
+            this.goodsSpecificationId = goodsSpecificationId;
+        }
+
+        public BigInteger getUnitId() {
+            return unitId;
+        }
+
+        public void setUnitId(BigInteger unitId) {
+            this.unitId = unitId;
+        }
+
+        public BigDecimal getPurchasePrice() {
+            return purchasePrice;
+        }
+
+        public void setPurchasePrice(BigDecimal purchasePrice) {
+            this.purchasePrice = purchasePrice;
+        }
+
+        public BigDecimal getQuantity() {
+            return quantity;
+        }
+
+        public void setQuantity(BigDecimal quantity) {
+            this.quantity = quantity;
+        }
     }
 }
--- src/main/java/build/dream/catering/services/PurchaseService.java
+++ src/main/java/build/dream/catering/services/PurchaseService.java
@@ -1,14 +1,68 @@
 package build.dream.catering.services;
 
 import build.dream.catering.models.purchase.SavePurchaseOrderModel;
+import build.dream.catering.utils.SequenceUtils;
 import build.dream.common.api.ApiRest;
+import build.dream.common.catering.domains.PurchaseOrder;
+import build.dream.common.catering.domains.PurchaseOrderDetail;
+import build.dream.common.utils.DatabaseHelper;
+import build.dream.common.utils.SerialNumberGenerator;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class PurchaseService {
     @Transactional(rollbackFor = Exception.class)
     public ApiRest savePurchaseOrder(SavePurchaseOrderModel savePurchaseOrderModel) {
-        return new ApiRest();
+        BigInteger tenantId = savePurchaseOrderModel.obtainTenantId();
+        String tenantCode = savePurchaseOrderModel.obtainTenantCode();
+        BigInteger branchId = savePurchaseOrderModel.obtainBranchId();
+        BigInteger userId = savePurchaseOrderModel.obtainUserId();
+        String remark = savePurchaseOrderModel.getRemark();
+        List<SavePurchaseOrderModel.Detail> details = savePurchaseOrderModel.getDetails();
+
+        String sequenceName = SerialNumberGenerator.generatorTodaySequenceName(tenantId, branchId, "purchase_order");
+        String orderNumber = SerialNumberGenerator.nextOrderNumber("JH", 4, SequenceUtils.nextValue(sequenceName));
+
+        PurchaseOrder purchaseOrder = PurchaseOrder.builder()
+                .tenantId(tenantId)
+                .tenantCode(tenantCode)
+                .branchId(branchId)
+                .orderNumber(orderNumber)
+                .originatorUserId(userId)
+                .remark(remark)
+                .build();
+
+        DatabaseHelper.insert(purchaseOrder);
+
+        BigInteger purchaseOrderId = purchaseOrder.getId();
+
+        List<PurchaseOrderDetail> purchaseOrderDetails = new ArrayList<PurchaseOrderDetail>();
+        for (SavePurchaseOrderModel.Detail detail : details) {
+            PurchaseOrderDetail purchaseOrderDetail = PurchaseOrderDetail.builder()
+                    .tenantId(tenantId)
+                    .tenantCode(tenantCode)
+                    .branchId(branchId)
+                    .purchaseOrderId(purchaseOrderId)
+                    .goodsId(detail.getGoodsId())
+                    .goodsSpecificationId(detail.getGoodsSpecificationId())
+                    .unitId(detail.getUnitId())
+                    .purchasePrice(detail.getPurchasePrice())
+                    .quantity(detail.getQuantity())
+                    .build();
+            purchaseOrderDetails.add(purchaseOrderDetail);
+        }
+        DatabaseHelper.insertAll(purchaseOrderDetails);
+
+        Map<String, Object> data = new HashMap<String, Object>();
+        data.put("purchaseOrder", purchaseOrder);
+        data.put("purchaseOrderDetails", purchaseOrderDetails);
+        return ApiRest.builder().data(data).message("保存进货单成功！").successful(true).build();
     }
 }
