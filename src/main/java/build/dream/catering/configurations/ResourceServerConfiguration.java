e61eb52a606436533bc9f3ad391a81f176ac2d7e
liuyandong <554030404@qq.com>
2018-11-27 17:47:46 +0800

    进货单接口开发！！

--- src/main/java/build/dream/catering/configurations/ResourceServerConfiguration.java
+++ src/main/java/build/dream/catering/configurations/ResourceServerConfiguration.java
@@ -10,6 +10,6 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/user/obtainBranchInfo", "/demo/**").permitAll().anyRequest().authenticated();
+        http.authorizeRequests().antMatchers("/favicon.ico", "/user/obtainBranchInfo", "/demo/**").permitAll().anyRequest().authenticated();
     }
 }
