c53dc888e61121313ae40bb431e1d86d219f0b8d
liuyandong <554030404@qq.com>
2018-12-07 17:32:51 +0800

    进货单接口开发！！

--- src/main/java/build/dream/catering/models/weixin/AuthCallbackModel.java
+++ src/main/java/build/dream/catering/models/weixin/AuthCallbackModel.java
@@ -2,6 +2,34 @@ package build.dream.catering.models.weixin;
 
 import build.dream.common.models.BasicModel;
 
-public class AuthCallbackModel extends BasicModel {
+import java.math.BigInteger;
 
+public class AuthCallbackModel extends BasicModel {
+    private BigInteger tenantId;
+    private String componentAppId;
+    private String authCode;
+
+    public BigInteger getTenantId() {
+        return tenantId;
+    }
+
+    public void setTenantId(BigInteger tenantId) {
+        this.tenantId = tenantId;
+    }
+
+    public String getComponentAppId() {
+        return componentAppId;
+    }
+
+    public void setComponentAppId(String componentAppId) {
+        this.componentAppId = componentAppId;
+    }
+
+    public String getAuthCode() {
+        return authCode;
+    }
+
+    public void setAuthCode(String authCode) {
+        this.authCode = authCode;
+    }
 }
