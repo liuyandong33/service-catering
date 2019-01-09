898f7d2084118f0d64a6c172b9e7cd7358ff59c2
liuyandong <554030404@qq.com>
2019-01-09 22:17:07 +0800

    elasticsearch 整合！！

--- src/main/java/build/dream/catering/models/goods/ObtainAllGoodsInfosModel.java
+++ src/main/java/build/dream/catering/models/goods/ObtainAllGoodsInfosModel.java
@@ -2,5 +2,18 @@ package build.dream.catering.models.goods;
 
 import build.dream.common.models.CateringBasicModel;
 
+import javax.validation.constraints.NotNull;
+import java.math.BigInteger;
+
 public class ObtainAllGoodsInfosModel extends CateringBasicModel {
+    @NotNull
+    private BigInteger categoryId;
+
+    public BigInteger getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(BigInteger categoryId) {
+        this.categoryId = categoryId;
+    }
 }
