dec817b5a690c257115bd68f9393738b9ac804af
liuyandong <554030404@qq.com>
2019-05-29 22:04:52 +0800

    菜牌功能开发！！

--- src/main/java/build/dream/catering/services/DataService.java
+++ src/main/java/build/dream/catering/services/DataService.java
@@ -54,37 +54,37 @@ public class DataService {
 
             List<DietOrderGroup> dietOrderGroups = JacksonUtils.readValueAsList(dietOrderGroupsJson, DietOrderGroup.class);
             Map<String, DietOrderGroup> dietOrderGroupMap = new HashMap<String, DietOrderGroup>();
-            for (DietOrderGroup dietOrderGroup : dietOrderGroups) {
+
+            dietOrderGroups.forEach(dietOrderGroup -> {
                 dietOrderGroup.setDietOrderId(dietOrderId);
                 dietOrderGroupMap.put(dietOrderGroup.getLocalId(), dietOrderGroup);
-            }
+            });
             DatabaseHelper.insertAll(dietOrderGroups);
 
             List<DietOrderDetail> dietOrderDetails = JacksonUtils.readValueAsList(dietOrderDetailsJson, DietOrderDetail.class);
             Map<String, DietOrderDetail> dietOrderDetailMap = new HashMap<String, DietOrderDetail>();
-            for (DietOrderDetail dietOrderDetail : dietOrderDetails) {
+
+            dietOrderDetails.forEach(dietOrderDetail -> {
                 DietOrderGroup dietOrderGroup = dietOrderGroupMap.get(dietOrderDetail.getLocalDietOrderGroupId());
                 dietOrderDetail.setDietOrderId(dietOrderId);
                 dietOrderDetail.setDietOrderGroupId(dietOrderGroup.getId());
                 dietOrderDetailMap.put(dietOrderDetail.getLocalId(), dietOrderDetail);
-            }
+            });
             DatabaseHelper.insertAll(dietOrderDetails);
 
             List<DietOrderPayment> dietOrderPayments = JacksonUtils.readValueAsList(dietOrderPaymentsJson, DietOrderPayment.class);
-            for (DietOrderPayment dietOrderPayment : dietOrderPayments) {
-                dietOrderPayment.setDietOrderId(dietOrderId);
-            }
+            dietOrderPayments.forEach(dietOrderPayment -> dietOrderPayment.setDietOrderId(dietOrderId));
             DatabaseHelper.insertAll(dietOrderPayments);
 
             if (StringUtils.isNotBlank(dietOrderDetailGoodsAttributesJson)) {
                 List<DietOrderDetailGoodsAttribute> dietOrderDetailGoodsAttributes = JacksonUtils.readValueAsList(dietOrderDetailGoodsAttributesJson, DietOrderDetailGoodsAttribute.class);
-                for (DietOrderDetailGoodsAttribute dietOrderDetailGoodsAttribute : dietOrderDetailGoodsAttributes) {
+                dietOrderDetailGoodsAttributes.forEach(dietOrderDetailGoodsAttribute -> {
                     DietOrderGroup dietOrderGroup = dietOrderGroupMap.get(dietOrderDetailGoodsAttribute.getLocalDietOrderGroupId());
                     DietOrderDetail dietOrderDetail = dietOrderDetailMap.get(dietOrderDetailGoodsAttribute.getLocalDietOrderDetailId());
                     dietOrderDetailGoodsAttribute.setDietOrderId(dietOrderId);
                     dietOrderDetailGoodsAttribute.setDietOrderGroupId(dietOrderGroup.getId());
                     dietOrderDetailGoodsAttribute.setDietOrderDetailId(dietOrderDetail.getId());
-                }
+                });
                 DatabaseHelper.insertAll(dietOrderDetailGoodsAttributes);
             }
 
