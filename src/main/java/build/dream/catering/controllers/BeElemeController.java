fc4567de04f46b1e96a6da488dbd53224185abe7
liuyandong <554030404@qq.com>
2019-06-02 23:05:25 +0800

    菜牌功能开发！！

---
+++ src/main/java/build/dream/catering/controllers/BeElemeController.java
@@ -0,0 +1,9 @@
+package build.dream.catering.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "beEleme")
+public class BeElemeController {
+}
