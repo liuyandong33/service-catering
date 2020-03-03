f29853b128c502b3e1f08986a84dbe7c9b678353
liuyandong <554030404@qq.com>
2020-03-04 03:32:57 +0800

    菜牌功能开发！！

--- src/main/java/build/dream/catering/Application.java
+++ src/main/java/build/dream/catering/Application.java
@@ -3,12 +3,13 @@ package build.dream.catering;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.kafka.annotation.EnableKafka;
 
 @ServletComponentScan
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableDiscoveryClient
 @EnableKafka
 @MapperScan(basePackages = {"build.dream.common.mappers", "build.dream.catering.mappers"})
